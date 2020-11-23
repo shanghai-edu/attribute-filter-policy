095f74759f16a59d96c5e4d5d0d6a5f891cfcf41
freedomkk-qfeng <freedomkk_qfeng@qq.com>
2020-11-23 15:01:38 +0800

    修改：      update_policy.sh

--- update_policy.sh
+++ update_policy.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-cd /home/attribute-filter-policy
-First=`md5sum /home/attribute-filter-policy/attribute-filter.xml | awk '{print $1}'`
+#### config ####
+policy_path='/home/attribute-filter-policy'
+idp_path='/opt/shibboleth-idp'
+
+################
+policy_xml=$policy_path'/attribute-filter.xml'
+idp_xml=$idp_path'/conf/attribute-filter.xml'
+
+cd $policy_path
+First=`md5sum $policy_xml | awk '{print $1}'`
 git pull
-Second=`md5sum /home/attribute-filter-policy/attribute-filter.xml | awk '{print $1}'`
+Second=`md5sum $idp_xml | awk '{print $1}'`
 if  [[ $First != $Second ]]
 then
-    /bin/cp -rf attribute-filter.xml /opt/shibboleth-idp/conf/attribute-filter.xml
+    /bin/cp -rf $policy_xml $idp_xml
     curl http://127.0.0.1:8080/idp/profile/admin/reload-service?id=shibboleth.AttributeFilterService
 fi
